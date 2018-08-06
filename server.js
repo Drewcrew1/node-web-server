c8c9c0040de861fe637ea474c2f8c88bf7712af9
Drew <drewharper29@gmail.com>
2018-08-06 15:04:02 -0400

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -62,4 +64,4 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(port);
